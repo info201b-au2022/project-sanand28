3b760577fda65bef56a5dcdee213c16fe6301670
sanand28 <shrana3@uw.edu>
2022-11-16 22:55:47 -0800

    summary info

--- source/sum_info.R
+++ source/sum_info.R
@@ -1,40 +1,55 @@
 # Summary
 # A source file that takes in a dataset and returns a list of info about it:
 
-aqi_2021 <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-sanand28/main/data/aqi_2021.csv")
-print(aqi_2021)
-View(aqi_2021)
-sum_info <- list()
-sum_info$num_observations <- nrow (aqi_2021)
-sum_info$AQI <- aqi_2021 %>%
-  filter(AQI == max(AQI, na.rm = T)) %>%
-  select(AQI)
+library(tidyverse)
 
-
-sum_info <- list()
-sum_info$num_observations <- nrow (aqi_2021)
-sum_info$Category <- aqi_2021 %>%
-  filter(Category == max(Category, na.rm = T)) %>%
-  select(Category)
-
+if (!exists("wildfires")) {
+  wildfires <- get_wildfires_df()
+}
+if (!exists("aqi")) {
+  aqi <- get_aqi_df()
+}
 
 sum_info <- list()
-sum_info$num_observations <- nrow (aqi_2021)
-sum_info$State.Name <- aqi_2021 %>%
-  filter(State.Name == max(State.Name, na.rm = T)) %>%
-  select(State.Name)
+
+# Info 1: number of wildfires
+sum_info$num_wildfires <- prettyNum(nrow(wildfires), big.mark = ",", scientific = FALSE)
+
+# Info 2: number of AQI observations
+sum_info$num_observations_aqi <- prettyNum(nrow(aqi), big.mark = ",", scientific = FALSE)
+
+# Info 3: average AQI in the U.S. every year from 2017 to 2021
+sum_info$average_AQI <- aqi %>%
+  mutate(Year = format(as.Date(Date), "%Y")) %>%
+  group_by(Year) %>%
+  summarize(average_aqi = mean(AQI)) %>%
+  mutate_if(is.numeric, round, digits = 2) %>%
+  pull(average_aqi) # round to 2 decimal places
 
 
-sum_info <- list()
-sum_info$num_observations <- nrow (aqi_2021)
-sum_info$county.Name <- aqi_2021 %>%
-  filter(county.Name == max(county.Name, na.rm = T)) %>%
-  select(county.Name)
+# Info 4: states with the worst AQI every year from 2017 to 2021
+install.packages("sf")
+install.packages("spData")
+source("data_access.R")
+get_AQI_df
+
+sum_info$worst_AQI <- aqi %>%
+  filter(AQI == max(AQI)) %>%
+  group_by(Year) %>%
+  pull(State.Name)
+
+    
+
+# Info 5: states with the best AQI every year from 2017 to 2021
+library("sf")
+library("spData")
+source("data_access.R")
+get_AQI_df
+
+sum_info$best_AQI <- aqi %>%
+  filter(AQI == min(AQI)) %>%
+  group_by(Year) %>%
+  pull(State.Name)
 
 
-sum_info <- list()
-sum_info$num_observations <- nrow (aqi_2021)
-sum_info$Date <- aqi_2021 %>%
-  filter(Date == max(Date, na.rm = T)) %>%
-  select(Date)
 
